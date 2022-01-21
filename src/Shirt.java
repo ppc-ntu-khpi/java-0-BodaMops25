611e576d20422ce962363c1529fb10c000a5e8cd
BodaMops25 <79203099+BodaMops25@users.noreply.github.com>
2022-01-21 17:14:32 +0000

    added screenshots

--- src/Shirt.java
+++ src/Shirt.java
@@ -1,17 +1,15 @@
 public class Shirt {
-  public int shirtID = 0; // стандартне значення номера моделі сорочки
-  public String description = "-description required-"; // стандартний опис сорочки
-  // коди кольорів: R=червоний, B=синій, G=зелений, U=невідомо
+  public int shirtID = 555;
+  public String description = "shirt description 1";
   public char colorCode = 'U';
-  public double price = 0.0; // стандартна вартість сорочки
-  public int quantityInStock = 0; // стандартна кількість на складі
+  public double price = 45.99;
+  public int quantityInStock = 0;
   
-  // цей метод просто виводить всю інформацію про сорочку на екран
   public void displayShirtInformation() {
     System.out.println("Shirt ID: " + shirtID);
     System.out.println("Shirt description:" + description);
     System.out.println("Color Code: " + colorCode);
     System.out.println("Shirt price: $" + price);
     System.out.println("Quantity in stock: " + quantityInStock);
-  } // кінець методу displayShirtInformation
-} // кінець опису класу
+  }
+}
